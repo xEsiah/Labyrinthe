b6a7c7d8007d9648aa25976062a4ac64c30cd178
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-01-28 11:01:42 +0100

    BASE FINALE (Boutons/HUD à ajouter)

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -3,71 +3,68 @@ from dicCoffres import dictionnaire_coffres
 from dicPieges import dictionnaire_pieges
 import random
 
-''' FONCTIONS '''
-
-def InterfaceLabyrinthe():
-    # Fenetre principale 
-    fenetre = Tk()
-    fenetre.attributes("-fullscreen", True)
-    fenetre.title("Labyrinthe")
-    
-    return fenetre
-
-def BaseLabyrinthe(largeur, hauteur, cellules, support):
-    # Centrer le canevas dans la fenêtre
-    frame = Frame(support)
-    frame.pack(fill=BOTH, expand=True)
-
-    # Canevas
-    canvas_base_labyrinthe = Canvas(
-        frame,
-        width=largeur,
-        height=hauteur,
-        bg="darkgray"
-    )
-    
-    canvas_base_labyrinthe.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-    entree_random = [90,180,330,480,840]
-    sortie_random = [90,180,330,480,840]
-    entree = random.choice(entree_random)
-    sortie = random.choice(sortie_random)
-
-    for y in range(0, hauteur, cellules):  # y = lignes
-        for x in range(0, largeur, cellules):  # x = colonnes
-            canvas_base_labyrinthe.create_rectangle(
-                x, y,
-                x + cellules, y + cellules,
-                fill="silver", outline="gray",     
-            )
-            if x == 0 or x == largeur-cellules or y == 0 or y == hauteur-cellules:
-                canvas_base_labyrinthe.create_rectangle(
-                x, y,
-                x + cellules, y + cellules,
-                fill="black", outline="gray",      
-            )       
-    canvas_base_labyrinthe.create_rectangle(
-        entree, 0,
-        entree + cellules, cellules,
-        fill="red", outline="gray",      
-    )      
-    canvas_base_labyrinthe.create_rectangle(
-        sortie, hauteur-cellules,
-        sortie + cellules, hauteur,
-        fill="blue", outline="gray",      
-    )
-    return 
-
 """ VARIABLES """
 
-fenetre = InterfaceLabyrinthe()
 largeur_ecran = 900
 hauteur_ecran = 900
 taille_cellule = 30
 entree = [30,180,330,480,870]
 sortie = [30,180,330,480,870]
 
+''' FONCTIONS & PROGRAMMES '''
+
+fenetre_jeu = Tk()
+fenetre_jeu.attributes("-fullscreen", True)
+fenetre_jeu.title("Labyrinthe")
+fond_fenetre = Frame(
+    fenetre_jeu,
+    background="orangered4",
+    border= 10,
+    relief="sunken",
+)
+fond_fenetre.pack(fill=BOTH, expand=True)
+
+dimension_labyrinthe = Canvas( 
+    fenetre_jeu,
+    width=largeur_ecran,
+    height=hauteur_ecran,
+    background="black",
+    border= -1,
+       relief="sunken",
+)
+dimension_labyrinthe.place(relx=0.5, rely=0.5, anchor=CENTER) # Placement du labyrinthe au centre de la fenètre 
+    
+for y in range(0, hauteur_ecran, taille_cellule):  # y = lignes
+    for x in range(0, largeur_ecran, taille_cellule):  # x = colonnes
+        dimension_labyrinthe.create_rectangle(
+            x, y,
+            x + taille_cellule, y + taille_cellule,
+            fill="silver", outline="gray",     
+        )
+        if x == 0 or x == largeur_ecran-taille_cellule or y == 0 or y == hauteur_ecran-taille_cellule:
+            dimension_labyrinthe.create_rectangle(
+            x, y,
+            x + taille_cellule, y + taille_cellule,
+               fill="gray3", outline="gray33",      
+        ) 
+
+# 25 configurations différentes pour l'entrée et la sortie
+entree_random = [90,180,330,480,840] 
+sortie_random = [90,180,330,480,840]
+entree = random.choice(entree_random)
+sortie = random.choice(sortie_random)
+dimension_labyrinthe.create_rectangle( # configuration entree
+    entree, 0,
+    entree + taille_cellule, taille_cellule,
+    fill="white", outline="gray",      
+)      
+dimension_labyrinthe.create_rectangle( # configuration sortie
+    sortie, hauteur_ecran-taille_cellule,
+    sortie + taille_cellule, hauteur_ecran,
+    fill="white", outline="gray",      
+)
+
+
 """ APPELS FONCTIONS """
 
-BaseLabyrinthe(largeur_ecran, hauteur_ecran, taille_cellule, fenetre)
-fenetre.mainloop()
+fenetre_jeu.mainloop()
