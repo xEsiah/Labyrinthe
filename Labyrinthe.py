1548b01f50d67465a4b706ca4795ff6a97ce2391
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-01-28 15:38:59 +0100

    OUAIS MON GARS

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -10,7 +10,8 @@ hauteur_ecran = 900
 taille_cellule = 30
 entree_random = [90,180,330,480,840] # Pour pouvoir plus tard définir l'entrée aléatoirement
 sortie_random = [90,180,330,480,840] # Pour pouvoir plus tard définir la sortie aléatoirement
-
+case_mystere = [dictionnaire_coffres, dictionnaire_pieges]
+murs = []
 
 ''' FONCTIONS & PROGRAMMES '''
 
@@ -49,31 +50,40 @@ for y in range(0, hauteur_ecran, taille_cellule):  # y = lignes
 entree = random.choice(entree_random)
 sortie = random.choice(sortie_random)
 
-def dessin_mur_et_sol(soloumur): # fonction pour dessiner les cases murs et les cases sols
-    if soloumur == 0:
+def dessin_terrain(terrain_à_générer): # fonction pour dessiner les cases murs et les cases sols
+    if terrain_à_générer == 0:
+        dimension_labyrinthe.create_rectangle(
+            mursy, mursx,
+            mursy + taille_cellule, mursx + taille_cellule,
+            fill="white", outline="darkgrey", 
+        ) 
+    elif terrain_à_générer == 1:
+        dimension_labyrinthe.create_rectangle(
+            mursy, mursx,
+            mursy + taille_cellule, mursx + taille_cellule,
+            fill="purple", outline="darkgrey", 
+        ) 
+    else: 
         dimension_labyrinthe.create_rectangle(
             mursy, mursx,
             mursy + taille_cellule, mursx + taille_cellule,
             fill="black", outline="darkgrey", 
         )
-    else: 
-        dimension_labyrinthe.create_rectangle(
-            mursy, mursx,
-            mursy + taille_cellule, mursx + taille_cellule,
-            fill="white", outline="darkgrey", 
-        )
     return 
     
-def genereation_murs_interns(x,proba): # fonction pour déterminer les probabilité de générer une case mur ou une case sol
-    if x < proba:
-        dessin_mur_et_sol(1)
+def generation_terrain(nombre_aléatoire,y,x): # fonction pour déterminer les probabilité de générer une case mur ou une case sol
+    if nombre_aléatoire < 50:
+        dessin_terrain(0)
+    elif nombre_aléatoire > 50 and nombre_aléatoire < 60:
+        dessin_terrain(1)
+        murs.append([x,y])
     else:
-        dessin_mur_et_sol(0)
-        
+        dessin_terrain(2)
+
 for mursy in range(30, hauteur_ecran-30, taille_cellule):  # y = lignes
     for mursx in range(30, largeur_ecran-30, taille_cellule):  # x = colonnes
         nombre = random.randint(0,100)
-        genereation_murs_interns(nombre, 55)
+        generation_terrain(nombre, mursy, mursx)
         dimension_labyrinthe.create_rectangle( # configuration entree
             entree, 0,
             entree + taille_cellule, taille_cellule*2,
@@ -84,7 +94,25 @@ for mursy in range(30, hauteur_ecran-30, taille_cellule):  # y = lignes
             sortie + taille_cellule, hauteur_ecran-taille_cellule*2,
             fill="white", outline="darkgrey",      
         )
-             
+print(murs)    
+    # for i in range (30, largeur_ecran-30, taille_cellule): 
+        
+    #     murs.append[i]
+    #     print(murs)
+
+
+# choix_case_mystere = random.choice(case_mystere)
+# print(choix_case_mystere)
+# choix_element_case_mystere = random.randint(choix_case_mystere)
+# print(choix_element_case_mystere)
+# dimension_labyrinthe.create_rectangle(
+#     mursy, mursx,
+#     mursy + taille_cellule, mursx + taille_cellule,
+#     fill="black", outline="darkgrey", 
+# )   
+        
+        
+        
 """ APPELS FONCTIONS """
 
 fenetre_jeu.mainloop()
