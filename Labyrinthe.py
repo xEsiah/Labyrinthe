037539f9f26b6db8a709ebfec8de6c021adb6d05
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-01-28 14:52:10 +0100

    OPTIMISATION

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -8,11 +8,13 @@ import random
 largeur_ecran = 900
 hauteur_ecran = 900
 taille_cellule = 30
-entree_random = [90,180,330,480,840] 
-sortie_random = [90,180,330,480,840]
+entree_random = [90,180,330,480,840] # Pour pouvoir plus tard définir l'entrée aléatoirement
+sortie_random = [90,180,330,480,840] # Pour pouvoir plus tard définir la sortie aléatoirement
+
 
 ''' FONCTIONS & PROGRAMMES '''
 
+# Création et paramétrage de la fenètre
 fenetre_jeu = Tk()
 fenetre_jeu.attributes("-fullscreen", True)
 fenetre_jeu.title("Labyrinthe")
@@ -24,6 +26,7 @@ fond_fenetre = Frame(
 )
 fond_fenetre.pack(fill=BOTH, expand=True)
 
+# Création et positionnement de la base du labyrinthe (Dimensions et murs extérieurs)
 dimension_labyrinthe = Canvas( 
     fenetre_jeu,
     width=largeur_ecran,
@@ -36,11 +39,6 @@ dimension_labyrinthe.place(relx=0.5, rely=0.5, anchor=CENTER) # Placement du lab
     
 for y in range(0, hauteur_ecran, taille_cellule):  # y = lignes
     for x in range(0, largeur_ecran, taille_cellule):  # x = colonnes
-        # dimension_labyrinthe.create_rectangle(
-        #     x, y,
-        #     x + taille_cellule, y + taille_cellule,
-        #     fill="silver", outline="gray",     
-        # )
         if x == 0 or x == largeur_ecran-taille_cellule or y == 0 or y == hauteur_ecran-taille_cellule:
             dimension_labyrinthe.create_rectangle(
             x, y,
@@ -48,51 +46,45 @@ for y in range(0, hauteur_ecran, taille_cellule):  # y = lignes
                fill="gray3", outline="gray33",      
         ) 
 
-# 25 configurations différentes pour l'entrée et la sortie
 entree = random.choice(entree_random)
 sortie = random.choice(sortie_random)
-dimension_labyrinthe.create_rectangle( # configuration entree
-    entree, 0,
-    entree + taille_cellule, taille_cellule,
-    fill="white", outline="gray",      
-)      
-dimension_labyrinthe.create_rectangle( # configuration sortie
-    sortie, hauteur_ecran-taille_cellule,
-    sortie + taille_cellule, hauteur_ecran,
-    fill="white", outline="gray",      
-)
 
-
-patternes_labyrinthe = [1,2,3,4,5,6]
-match(patternes_labyrinthe):
-    case 1:
-        for i in range(12):
-            dimension_labyrinthe.create_rectangle(
-                i, i,
-                i + taille_cellule, i + taille_cellule,
-                fill="grey85", outline="white", 
-            )
-        
-
+def dessin_mur_et_sol(soloumur): # fonction pour dessiner les cases murs et les cases sols
+    if soloumur == 0:
+        dimension_labyrinthe.create_rectangle(
+            mursy, mursx,
+            mursy + taille_cellule, mursx + taille_cellule,
+            fill="black", outline="darkgrey", 
+        )
+    else: 
+        dimension_labyrinthe.create_rectangle(
+            mursy, mursx,
+            mursy + taille_cellule, mursx + taille_cellule,
+            fill="white", outline="darkgrey", 
+        )
+    return 
     
-murs = [0,1]
-placement_murs = random.choice(murs)
-
-for mursy in range(30, hauteur_ecran-30, taille_cellule*4):  # y = lignes
-    for mursx in range(30, largeur_ecran-30, taille_cellule*4):  # x = colonnes
-        placement_murs = random.choice(murs)
-        if placement_murs == 1 :
-            placement_patternes = random.choices(patternes_labyrinthe)
-            
-        else:
-            dimension_labyrinthe.create_rectangle(
-                mursx, mursy,
-                mursx + taille_cellule, mursy + taille_cellule,
-                fill="grey85", outline="white", 
-            )
-            
+def genereation_murs_interns(x,proba): # fonction pour déterminer les probabilité de générer une case mur ou une case sol
+    if x < proba:
+        dessin_mur_et_sol(1)
+    else:
+        dessin_mur_et_sol(0)
         
-
+for mursy in range(30, hauteur_ecran-30, taille_cellule):  # y = lignes
+    for mursx in range(30, largeur_ecran-30, taille_cellule):  # x = colonnes
+        nombre = random.randint(0,100)
+        genereation_murs_interns(nombre, 55)
+        dimension_labyrinthe.create_rectangle( # configuration entree
+            entree, 0,
+            entree + taille_cellule, taille_cellule*2,
+            fill="white", outline="darkgrey",      
+        )
+        dimension_labyrinthe.create_rectangle( # configuration sortie
+            sortie, hauteur_ecran,
+            sortie + taille_cellule, hauteur_ecran-taille_cellule*2,
+            fill="white", outline="darkgrey",      
+        )
+             
 """ APPELS FONCTIONS """
 
 fenetre_jeu.mainloop()
