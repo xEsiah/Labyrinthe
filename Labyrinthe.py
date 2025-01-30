ab6d76a6de885145d1c1b0ebfca8c11df614d190
xEsiah-LAPTOP <theo.thws.06@gmail.com>
2025-01-30 13:33:56 +0100

    Récupération  des coordonnées des murs et optimisation du code

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random
 
+
 """ VARIABLES """
 
 largeur_ecran = 900
@@ -9,34 +10,23 @@ taille_cellule = 30
 entree_random = [90,180,330,480,840] # Pour pouvoir plus tard définir l'entrée aléatoirement
 sortie_random = [90,180,330,480,840] # Pour pouvoir plus tard définir la sortie aléatoirement
 
+
 """ DICTIONNAIRES ET LISTES """
 
-dictionnaire_pieges = [
-    ["Piques",[0, 1, 2, 3, 4, 5]], 
-    ["Trappes",[0, 1, 2, 3, 4, 5]], 
-    ["Pièges à ours",[0, 1, 2, 3, 4, 5]], 
-    ["Flèches empoisonnées",[0, 1, 2, 3, 4, 5]], 
-    ["Plaques piégées",[0, 1, 2, 3, 4, 5]],
-    ]
-dictionnaire_coffres = [
-    ["Carte",[0, 1]], 
-    ["Potion de soin",[0, 1]], 
-    ["Plaque d'armure",[0, 1, 2]]
-]
-personnage = {
-    "Points de vie": 0,
-    "Rapidité": 0,
-    "Vision": 0,
-}
 
-
-""" Liste cases """
-
-liste_murs = []
-liste_cases_mystère = []
-liste_case_sol = []
+dictionnaire_pieges = [["Piques"],["Trappes"],["Pièges à ours"],["Flèches empoisonnées"],["Plaques piégées"]]
+dictionnaire_coffres = [["Carte"], ["Potion de soin"], ["Plaque d'armure"]]
+lc_murs = []
 liste_evenements = []
 case_mystere = [dictionnaire_coffres, dictionnaire_pieges]
+personnage = {
+    "Points de vie": 10,
+    "Rapidité": 0,
+    "Vision": 0,
+    "Plaque d'armure" : 0,
+    "Potion de soin" : 0,
+}
+
 
 ''' FONCTIONS & PROGRAMMES '''
 
@@ -52,7 +42,9 @@ fond_fenetre = Frame(
 )
 fond_fenetre.pack(fill=BOTH, expand=True)
 
+
 ''' Création et positionnement de la base du labyrinthe (Dimensions et murs extérieurs) '''
+
 dimension_labyrinthe = Canvas( 
     fenetre_jeu,
     width=largeur_ecran,
@@ -75,47 +67,48 @@ for y in range(0, hauteur_ecran, taille_cellule):  # y = lignes
 entree = random.choice(entree_random)
 sortie = random.choice(sortie_random)
 
+
 ''' Creation murs intérieurs (randomisées) et cases sols dont cases objets '''
-def dessin_terrain(terrain_à_générer): # fonction pour dessiner les cases murs et les cases sols
-    if terrain_à_générer == 0:
+
+def dessin_des_trois_types_de_terrain(terrain_à_générer): # fonction pour dessiner les cases murs et les cases sols
+    if terrain_à_générer == 0: # sols
         dimension_labyrinthe.create_rectangle(
-            mursy, mursx,
-            mursy + taille_cellule, mursx + taille_cellule,
+            mursx, mursy,
+            mursx + taille_cellule, mursy + taille_cellule,
             fill="white", outline="darkgrey", 
         ) 
-    elif terrain_à_générer == 1:
+    elif terrain_à_générer == 1: # objets/pièges
         dimension_labyrinthe.create_rectangle(
-            mursy, mursx,
-            mursy + taille_cellule, mursx + taille_cellule,
+            mursx, mursy,
+            mursx + taille_cellule, mursy + taille_cellule,
             fill="purple", outline="darkgrey", 
         ) 
-    else: 
+    else: # murs
         dimension_labyrinthe.create_rectangle(
-            mursy, mursx,
-            mursy + taille_cellule, mursx + taille_cellule,
+            mursx, mursy,
+            mursx + taille_cellule, mursy + taille_cellule,
             fill="black", outline="darkgrey", 
         )
     return 
     
-def generation_terrain(nombre_aléatoire,y,x,mystere): # fonction pour déterminer les probabilité de générer une case mur ou une case sol
-    if nombre_aléatoire < 50:
-        dessin_terrain(0)
-        liste_murs.append([x,y])
-    elif nombre_aléatoire > 50 and nombre_aléatoire < 60:
-        dessin_terrain(1)
-        
-            
-            # randomizer les pieges et le nombre 
-        liste_cases_mystère.append([x,y])
-    else:
-        dessin_terrain(2)
-        liste_case_sol.append([x,y])
-    return 
+def generation_terrain(nombre_aléatoire,x,y,mystere): # fonction pour déterminer les probabilité de générer une case mur ou une case sol
+    global lc_murs
+    if nombre_aléatoire <= 51: # sols
+        dessin_des_trois_types_de_terrain(0)
+    elif nombre_aléatoire >= 52 and nombre_aléatoire < 60: # objets/pièges
+        dessin_des_trois_types_de_terrain(1)
+        # randomizer les pieges et le nombre 
+        choix_case_mystere = random.choice(mystere)
+        random.shuffle(choix_case_mystere)
+    else: # murs
+        dessin_des_trois_types_de_terrain(2)
+        lc_murs.append([x,y])
+    return lc_murs
 
-for mursy in range(30, hauteur_ecran-30, taille_cellule):  # y = lignes
-    for mursx in range(30, largeur_ecran-30, taille_cellule):  # x = colonnes
+for mursx in range(30, hauteur_ecran-30, taille_cellule):  # y = lignes
+    for mursy in range(30, largeur_ecran-30, taille_cellule):  # x = colonnes
         nombre = random.randint(0,100)
-        generation_terrain(nombre, mursy, mursx,case_mystere)
+        generation_terrain(nombre, mursx, mursy,case_mystere)
         dimension_labyrinthe.create_rectangle( # configuration entree
             entree, 0,
             entree + taille_cellule, taille_cellule*2,
@@ -127,35 +120,38 @@ for mursy in range(30, hauteur_ecran-30, taille_cellule):  # y = lignes
             fill="white", outline="darkgrey",      
         )
 
+
 ''' Fonction pour insérer et déplacer le personnage dans le labyrinthe '''
-rectangle = dimension_labyrinthe.create_rectangle(
+
+personnage = dimension_labyrinthe.create_rectangle(
     entree + 5, 5,  # Départ légèrement à l'intérieur de l'entrée
     entree + taille_cellule - 5, taille_cellule - 5,
     fill="aqua", outline="black"
 )
 
-def move_rectangle(event):
+def deplacement_personnage(event):
     if event.keysym == "Up":
-        dimension_labyrinthe.move(rectangle, 0, -taille_cellule)
+        dimension_labyrinthe.move(personnage, 0, -taille_cellule)
     elif event.keysym == "Down":
-        dimension_labyrinthe.move(rectangle, 0, taille_cellule)
+        dimension_labyrinthe.move(personnage, 0, taille_cellule)
     elif event.keysym == "Left":
-        dimension_labyrinthe.move(rectangle, -taille_cellule, 0)
+        dimension_labyrinthe.move(personnage, -taille_cellule, 0)
     elif event.keysym == "Right":
-        dimension_labyrinthe.move(rectangle, taille_cellule, 0)
+        dimension_labyrinthe.move(personnage, taille_cellule, 0)
         
-fenetre_jeu.bind("<Up>", move_rectangle)
-fenetre_jeu.bind("<Down>", move_rectangle)
-fenetre_jeu.bind("<Left>", move_rectangle)
-fenetre_jeu.bind("<Right>", move_rectangle)
-
-
-# print("MURS\n",liste_murs) 
-# print("Mystere\n",liste_cases_mystère)
-# print("Passage\n",liste_cases_passage)
-# print(len(liste_cases_mystère+liste_cases_passage+liste_murs))
-
         
 """ APPELS FONCTIONS """
 
+
+fenetre_jeu.bind("<Up>", deplacement_personnage)
+fenetre_jeu.bind("<Down>", deplacement_personnage)
+fenetre_jeu.bind("<Left>", deplacement_personnage)
+fenetre_jeu.bind("<Right>", deplacement_personnage)
+print(lc_murs,len(lc_murs))
 fenetre_jeu.mainloop()
+
+
+# x1 + taille_cellule = verif droite
+# x1 - taille _cellule + verif gauche
+# y1 + taille_cellule = verif bas
+# y1 - taille_cellule = verif haut
